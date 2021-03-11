be6985ce62c5d036e13d9459887f1f6a3df5596c
mattscodeans <wang4222@purdue.edu>
2021-03-10 17:49:24 -0800

    Added ability to use cpp external modules.

---
+++ modules/cppModuleRunner.py
@@ -0,0 +1,35 @@
+from command import Command
+import discord
+import os
+import subprocess
+
+#Function to test sending data to external commands
+
+# Every module has to have a command list
+commandList = []
+
+# Command uses !cpp file-name command-name (any arguments)
+# First compile the code into a .exe file
+commandList.append(Command("!cpp", "cppRunner", "Runs a module that uses cpp. Format: !cpp file-name command-name (any arguments)"))
+async def cppRunner(client, message):
+    if len(message.content.split(" ")) < 3:
+        response = "Incorrect usage! Check !help for usage info."
+        embed = discord.Embed(title='!cpp Incorrect Usage', description=response, colour=discord.Colour.red())
+        await message.channel.send(embed=embed)
+        return
+    # Extract file name and command name
+    fileName = message.content.split(" ")[1]
+    if fileName.endswith(".exe") == False:
+        fileName += ".exe"
+    commandName = message.content.split(" ")[2]
+    for nativeFile in os.listdir("modules"):
+        if nativeFile == fileName:
+            # Important security consideration: Always use run with parameter shell=False, which is done
+            # by default. Not doing so allows for shell injection.
+            result = subprocess.run(["./modules/" + fileName, commandName], capture_output=True, text=True)
+            output = result.stdout
+            # Send the output recieved from the program via a message
+            embed = discord.Embed(title= commandName + " Output", description=output, colour=discord.Colour.gold())
+            await message.channel.send(embed=embed)
+            
+    
---
+++ modules/cppTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void cppTest() {
+	cout << "This was ran from a CPP file!" << endl;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		cout << "Incorrect use!" << endl;
+		return 1;
+	}
+	string command = argv[1];
+	if (command == "!cppTest") {
+		cppTest();
+		return 0;
+	}
+	cout << "Incorrect use!" << endl;
+	return 0;
+}
