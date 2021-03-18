43df3bcc4bdbe3b98a8d968c87c43966850c16d5
Kyle Arrowood <karrowo@purdue.edu>
2021-03-18 18:17:36 -0400

    Started working on the webscraping for images, had trouble working with google so I used bing and the picture quality is not great right now.

--- modules/googleModule.py
+++ modules/googleModule.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from command import Command
 import requests
 import discord
+import urllib,urllib2
 from googlesearch import search 
 
 
@@ -16,24 +17,32 @@ async def googleSearch(client, message):
     if len(contents) == 1:
         await message.channel.send("Please give an argument to be searched on google.")
     
-    embed = discord.Embed(title = "Google", colour = discord.Colour.green())
-    embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+    start = 1
+    if (contents[1].isnumeric() or contents[1] == "images"):
+        start = 2
     # to search 
     query = ""
-    for i in range(1, len(contents)-1):
-        if (contents[1].isnumeric()):
-            i += 1
+    for i in range(start, len(contents)-1):
         query += contents[i] + "+"
     query += contents[len(contents)-1]
     searchURL = "https://www.google.com/search?q=" + query
     html = requests.get(searchURL)
     soup = BeautifulSoup(html.content, 'html.parser')
-
+    embed = discord.Embed(title = query, colour = discord.Colour.blue())
+    embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
     if (contents[1].isnumeric()):
         numResults = int(contents[1])
         for i in search(query.replace("+", " "), tld="co.in", num=numResults, stop=numResults, pause=2):
-            s = str(i)
-            embed.add_field(name="Result", value=s, inline=False)
+            embed.add_field(name='\u200b', value=i, inline=False)
+    elif (contents[1] == "images"):
+        searchURL = "https://www.bing.com/images/search?q=" + query + "&first=1&tsc=ImageHoverTitle"
+        html = requests.get(searchURL)
+        soup = BeautifulSoup(html.content, 'html.parser')
+        # Show first 5 images
+        images = soup.find_all('img', class_='mimg')
+        for image in images:
+            src = image.attrs['src']
+            embed.add_field(name='\u200b', value=src, inline=False)
     else:
         description = soup.find_all('div', class_="BNeawe s3v9rd AP7Wnd")
         result = description[0].text
--- testGoogle.py
+++ testGoogle.py
@@ -12,25 +12,37 @@ from googlesearch import search
 # contents = message.split(" ")
 
 # to search 
-query = "How+large+is+earth"
+query = "dogs"
 
-searchURL = "https://www.google.com/search?q=" + query
+# searchURL = "https://www.bing.com/images/search?q=" + query
+searchURL = "https://www.bing.com/images/search?q=" + query + "&first=1&tsc=ImageHoverTitle"
 print(searchURL)
 html = requests.get(searchURL)
 soup = BeautifulSoup(html.content, 'html.parser')
 
-for i in search(query.replace("+", " "), tld="co.in", num=10, stop=10, pause=2):
-    print(i)
-    index = i.find("www.")
-    name = i[index:]
-    print(name)
+# for i in search(query.replace("+", " "), tld="co.in", num=10, stop=10, pause=2):
+#     print(i)
+#     index = i.find("www.")
+#     name = i[index:]
+#     print(name)
 
 
 ## This may be used for ones with no description
-description = soup.find_all('div', class_="BNeawe s3v9rd AP7Wnd")
+#description = soup.find_all('div', class_="BNeawe s3v9rd AP7Wnd")
+#description = soup.find_all('div')
 #, class_='PZPZlf hb8SAc'
-print(description[0].text)
+
+#for i in description:
+#    print(i.text + "\n")
+
 # if description:
 #     print(description)
 # else:
 #     print("No description")
+
+# div = soup.find('div', id='vm_c')
+images = soup.find_all('img', class_='mimg')
+# print (images)
+src = images[0].attrs['src']
+print(src)
+
