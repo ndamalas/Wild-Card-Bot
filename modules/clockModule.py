773353c58d81dfb8e882d09735363fc2b26693b6
Kyle Arrowood <karrowo@purdue.edu>
2021-03-24 17:28:21 -0400

    Added ability to pause and unpause a timer with a command.

--- modules/clockModule.py
+++ modules/clockModule.py
@@ -18,7 +18,14 @@ async def stopwatch(client, message):
 # while int(time.time() - start) < int(10):
 #     time.sleep(1)
 #     print(math.floor(time.time() - start))
-
+# Helper to get the most recent timer
+async def getLastTimer(client, message):
+    hist = await message.channel.history(limit=30).flatten()
+    for old in hist:
+        if old.author == client.user and (old.content.find("remaining.") != -1):
+            lastTimer = old
+            break
+    return lastTimer
 
 commandList.append(Command("!timer", "timer", "TODO"))
 async def timer(client, message):
@@ -27,29 +34,41 @@ async def timer(client, message):
         await message.channel.send("Please specify the amount of time you want on your timer and whether it is in minutes or seconds.\nUsage: !timer <min/sec> <TIME>")
         return
     if content[1] == 'delete':
-        hist = await message.channel.history(limit=30).flatten()
-        for old in hist:
-            if old.author == client.user and (old.content.find("remaining.") != -1):
-                lastTimer = old
-                print(lastTimer.content)
+        lastTimer = await getLastTimer(client, message)
         await lastTimer.delete()
         await message.channel.send("Timer successfully deleted.")
         return
-    if content[1] == 'pause':
-        async for old in message.channel.history(limit=5):
-            if old.author == client.user and old.content.find("remaining."):
-                lastTimer = old
+    elif content[1] == 'pause':
+        lastTimer = await getLastTimer(client, message)
         timeLeft = lastTimer.content.split(" ")[0]
-        print("timeleft=" + timeLeft)
         await lastTimer.delete()
-        await message.channel.send("Timer successfully paused.")
+        await message.channel.send(content="{} remaining.".format(timeLeft))
+        return
+    elif content[1] == 'unpause':
+        lastTimer = await getLastTimer(client, message)
+        left = lastTimer.content.split(" ")[0]
+        start = time.time()
+        end = float(left.split(":")[0]) + (float(left.split(":")[1]) / 60)
+        while int(time.time() - start) < math.floor(float(end) * 60):
+            secondsLeft = math.floor(float(end) * 60) - math.floor(time.time() - start)
+            if (secondsLeft % 60 < 10):
+                left = str(math.floor(secondsLeft / 60)) + ":0" + str(secondsLeft % 60)
+            else:
+                left = str(math.floor(secondsLeft / 60)) + ":" + str(secondsLeft % 60)
+            try:
+                await lastTimer.edit(content="{} remaining.".format(left))
+            except:
+                return
+            time.sleep(0.95)
+        await lastTimer.edit(content="0:00 remaining.")
+        await message.channel.send(message.author.mention + ", your timer is complete.")
         return
         
         
-    if not(content[1] == "min" or content[1] == "sec"):
-        await message.channel.send("Please specify the amount of time you want on your timer and whether it is in minutes or seconds.\nUsage: !timer <min/sec> <TIME>")
-        return
-    end = content[2]
+    # if not(content[1] == "min" or content[1] == "sec"):
+    #     await message.channel.send("Please specify the amount of time you want on your timer and whether it is in minutes or seconds.\nUsage: !timer <min/sec> <TIME>")
+    #     return
+    end = content[1]
     def is_number(s):
         if s.isnumeric():
             return True
@@ -62,30 +81,30 @@ async def timer(client, message):
         await message.channel.send("Please enter an amount of time to run a timer.")
         return
     start = time.time()
-    if content[1] == 'sec':
-        newMessage = await message.channel.send("{} seconds remaining.".format(end))
-        while int(time.time() - start) < math.floor(float(end)):
-            await newMessage.edit(content="{} seconds remaining.".format(int(math.floor(float(end))) - math.floor(time.time() - start)))
-            time.sleep(0.95)
+    # if content[1] == 'sec':
+    #     newMessage = await message.channel.send("{} seconds remaining.".format(end))
+    #     while int(time.time() - start) < math.floor(float(end)):
+    #         await newMessage.edit(content="{} seconds remaining.".format(int(math.floor(float(end))) - math.floor(time.time() - start)))
+    #         time.sleep(0.95)
+    #     try:
+    #         await newMessage.edit(content="0 seconds remaining.")
+    #     except:
+    #         return
+    #elif content[1] == 'min':
+    left = "0:00"
+    newMessage = await message.channel.send("Starting your timer.")
+    while int(time.time() - start) < math.floor(float(end) * 60):
+        secondsLeft = math.floor(float(end) * 60) - math.floor(time.time() - start)
+        if (secondsLeft % 60 < 10):
+            left = str(math.floor(secondsLeft / 60)) + ":0" + str(secondsLeft % 60)
+        else:
+            left = str(math.floor(secondsLeft / 60)) + ":" + str(secondsLeft % 60)
         try:
-            await newMessage.edit(content="0 seconds remaining.")
+            await newMessage.edit(content="{} remaining.".format(left))
         except:
             return
-    elif content[1] == 'min':
-        left = "0:00"
-        newMessage = await message.channel.send("Starting your timer.")
-        while int(time.time() - start) < math.floor(float(end) * 60):
-            secondsLeft = math.floor(float(end) * 60) - math.floor(time.time() - start)
-            if (secondsLeft % 60 < 10):
-                left = str(math.floor(secondsLeft / 60)) + ":0" + str(secondsLeft % 60)
-            else:
-                left = str(math.floor(secondsLeft / 60)) + ":" + str(secondsLeft % 60)
-            try:
-                await newMessage.edit(content="{} remaining.".format(left))
-            except:
-                return
-            time.sleep(0.95)
-        await newMessage.edit(content="0:00 remaining.")
+        time.sleep(0.95)
+    await newMessage.edit(content="0:00 remaining.")
     await message.channel.send(message.author.mention + ", your timer is complete.")
 
 
