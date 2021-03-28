8db756b20b2f8188993e7c408ede74e7a1948f75
KonstantinLiehr <54676578+KonstantinLiehr@users.noreply.github.com>
2021-03-28 06:58:28 -0400

    Got rid of file collision on my end

--- modules/riot.py
+++ modules/riot.py
@@ -6,27 +6,8 @@ from riotwatcher import LolWatcher, ApiError, RiotWatcher, LorWatcher, TftWatche
 
 commandList = []
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-riot_api_key = "RGAPI-654cbb73-cbb2-4bfb-9437-2b73fda6b689"
-=======
+
 riot_api_key = "RGAPI-699f4344-49e5-4e99-8a76-362b6a39c1ad"
->>>>>>> Stashed changes
-=======
-riot_api_key = "RGAPI-699f4344-49e5-4e99-8a76-362b6a39c1ad"
->>>>>>> Stashed changes
-=======
-riot_api_key = "RGAPI-699f4344-49e5-4e99-8a76-362b6a39c1ad"
->>>>>>> Stashed changes
-=======
-riot_api_key = "RGAPI-699f4344-49e5-4e99-8a76-362b6a39c1ad"
->>>>>>> Stashed changes
-=======
-riot_api_key = "RGAPI-699f4344-49e5-4e99-8a76-362b6a39c1ad"
->>>>>>> Stashed changes
 lol_watcher = LolWatcher(riot_api_key)
 riot_watcher = RiotWatcher(riot_api_key)
 lor_watcher = LorWatcher(riot_api_key)
