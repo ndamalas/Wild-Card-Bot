258be20e52176cf528807c4d88669964c4670f95
Kyle Arrowood <karrowo@purdue.edu>
2021-03-30 10:51:40 -0400

    Started working on the timezone features, able to print out local times in different timezones.

--- modules/clockModule.py
+++ modules/clockModule.py
@@ -6,6 +6,11 @@ import time
 import math
 import threading
 import asyncio
+import pytz
+from datetime import datetime
+from bs4 import BeautifulSoup
+import requests
+from timezonefinder import TimezoneFinder
 
 #Function to test sending data to external commands
 
@@ -219,4 +224,36 @@ async def timer(client, message):
 
 commandList.append(Command("!timezone", "timezone", "TODO"))
 async def timezone(client, message):
-    pass
+    messageContents = message.content.split(" ")
+    # First webscrape coordinates
+    searchURL = "https://www.google.com/search?q=" + messageContents[1:] + "+coordinates"
+    html = requests.get(searchURL)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    description = soup.find_all('div', class_="BNeawe iBp4i AP7Wnd")
+    if len(description) == 0:
+        description = soup.find_all('div', class_="BNeawe s3v9rd AP7Wnd")
+    result = description[0].text
+    #print(result)
+    contents = result.split(" ")
+
+    # Next plug coordinates into api
+    tf = TimezoneFinder()
+    latitude, longitude = float(contents[0][:-1]), float(contents[2][:-1])
+    if contents[1] == "S,":
+        latitude = -latitude
+    if contents[3] == "W":
+        longitude = -longitude
+    location = tf.timezone_at(lng=longitude, lat=latitude)
+
+    #print(location)
+
+    # Get local time
+    t = pytz.timezone(location)
+
+    newTime = datetime.now(t)
+
+    curr_clock = newTime.strftime("%a, %d %b %Y %H:%M:%S")
+    #print(curr_clock)
+
+    await message.channel.send("In the timezone: " + location + "\nLocal time is: " + curr_clock)
