ec45b937a60af51553676b16c59d332a2587eba1
mgu247 <gu247@purdue.edu>
2021-04-09 03:25:13 -0700

    Functionality to display Steam user status

--- modules/steam.py
+++ modules/steam.py
@@ -11,11 +11,46 @@ steam_api_key = "54964BFA9E709E047EB70CC7A6F2BA1C"
 
 commandList.append(Command("!steam", "get_status", ""))
 async def get_status(ctx, message):
-    session = aiohttp.ClientSession()
     name = message.content.split(" ")[1]
+
+    # Get steamID from name
+    session = aiohttp.ClientSession()
     output = await session.get(f"https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={steam_api_key}&vanityurl={name}")
     out = await output.json()
     await session.close()
     myDf = pd.DataFrame.from_dict(out)
-    print(myDf['response'][myDf.index == "steamid"].item())
-    
+    steamid = myDf['response'][myDf.index == "steamid"].item()
+
+    # Get user infos
+    session = aiohttp.ClientSession()
+    output = await session.get(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={steam_api_key}&steamids={steamid}")
+    out = await output.json()
+    await session.close()
+    pd.set_option("display.max_columns", None)
+    userDf = pd.DataFrame.from_dict(out)
+    userDf = userDf['response'][userDf.index == "players"][0][0]
+
+    # Output user status
+    await message.channel.send(userDf['personaname'] + " is currently " + status(userDf['personastate']))
+
+
+def status(state):
+    s = {
+        0: "Offline",
+        1: "Online",
+        2: "Busy",
+        3: "Away",
+        4: "Snooze",
+        5: "Looking to trade",
+        6: "Looking to play",
+    }
+    return s.get(state)
+
+def unnest(myDf: pd.DataFrame, columns: list) -> pd.DataFrame:
+  tempDf = pd.DataFrame()
+  for i in columns:
+    if i in myDf:
+      if isinstance(myDf.loc[:, i].iloc[0], dict):
+        x = myDf[i].apply(pd.Series)
+        tempDf = pd.concat([tempDf, x], axis = 1)
+  return tempDf
