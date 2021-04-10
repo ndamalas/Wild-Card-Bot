6439fd73e9f7f9a5e1e6006d5e408ebd2b6b8f45
mattscodeans <wang4222@purdue.edu>
2021-04-10 01:05:32 -0700

    Added functionality to draw two cards per player in the game at the start of the round and send each player a direct message with their hand

--- modules/blackjackModule.py
+++ modules/blackjackModule.py
@@ -66,9 +66,14 @@ class Player:
     # Edit the amount of money the player has
     def updateBalance(self, change):
         self.money += change
+    # Draw card
+    def drawCard(self):
+        self.hand.append(randomCard())
 
 # Class representing a game
 class Game:
+    actions = ["Hit", "Stand", "Fold"]
+    actionReactions = [u"\u2705",  	u"\u274E", u"\U0001F6AB"]
     def __init__(self, guild, tc):
         self.guild = guild  # The server of the game
         self.players = []   # List of Player objects that represent the players in the game
@@ -108,7 +113,7 @@ class Game:
         for player in self.players:
             playerObj = await self.guild.fetch_member(player.userid)
             playerStr += playerObj.display_name
-            playerStr += "\nMoney: " + str(player.money) + "\n\n"
+            playerStr += "\nMoney: $" + str(player.money) + "\n\n"
         embed.add_field(name="Players", value=playerStr, inline=False)
         embed.add_field(name="Round Starting", value="Check your DMs! The round is starting!", inline=False)
         self.embed = embed
@@ -183,18 +188,33 @@ async def runRounds(client, game):
         # Send each player a chance to change bets
         for player in game.players:
             await sendBetChanger(client, game, player)
+        # Wait for all players to finish
+        while len([p for p in game.players if p.done == True]) < len(game.players):
+            pass
+        # Reset all players
+        for player in game.players:
+            player.reset()
+        # Draw two cards for each player
+        for player in game.players:
+            player.drawCard()
+            player.drawCard()
+        # Send each player the prompt for hit, stand, or fold
+        for player in game.players:
+            await sendHand(client, game, player)
         break
 
 # Direct messages the bet changing message to the player
 async def sendBetChanger(client, game, player):
     response = "The round is about to begin!\n"
-    response += "Current Bet: **$" + str(player.money) + "\n"
+    response += "Current Bet: **$" + str(player.bet) + "**\n"
     response += "To change your bet amount, react to this message.\n"
-    response += "You have 15 seconds to make a decision\n"
+    response += "You have 15 seconds to make a decision.\n"
+    embed = discord.Embed(title="Round Starting", description=response, colour=discord.Colour.blurple())
     # Only bet amounts allowed: 100, 200, 500, 1000
+    betStr = ""
     for i in range(len(Player.betAmounts)):
-        response += Player.betReactions[i] + " for $" + str(Player.betAmounts[i]) + "\n"
-    embed = discord.Embed(title="Round Starting", description=response, colour=discord.Colour.dark_gray())
+        betStr += Player.betReactions[i] + " for $" + str(Player.betAmounts[i]) + "\n"
+    embed.add_field(name="Bets", value=betStr, inline=False)
     playerObj = await game.guild.fetch_member(player.userid)
     betMsg = await playerObj.send(embed=embed)
     # React with all the bet amounts
@@ -215,5 +235,35 @@ async def sendBetChanger(client, game, player):
             if str(reaction.emoji) == Player.betReactions[i]:
                 player.bet = Player.betAmounts[i]
                 break
-    print(player.bet)
     await betMsg.delete()
+    response = "Your bet amount: **$" + str(player.bet) + "**\n"
+    embed = discord.Embed(title="Your Bet", description=response, colour=discord.Colour.blurple())
+    await playerObj.send(embed=embed)
+    # Notify player is done
+    player.done = True
+
+# Direct messages the player's hand
+async def sendHand(client, game, player):
+    response = "**Round " + str(game.round) + "**\n"
+    response += "Here are your cards. React to hit, stand, or fold.\n"
+    response += "You have 30 seconds to make a decision.\n"
+    response += "**Your Cards:**"
+    embed = discord.Embed(title="Your Hand", description=response, colour=discord.Colour.dark_gray())
+    # Display the cards
+    cardNum = 1
+    for card in player.hand:
+        embed.add_field(name="Card " + str(cardNum), value=str(card), inline=True)
+        cardNum += 1
+    # Calculate the hand value
+    player.calculateValue()
+    embed.add_field(name="Hand Value", value=str(player.value), inline=False)
+    # Display the actions
+    actionStr = ""
+    for i in range(len(Game.actions)):
+        actionStr += Game.actionReactions[i] + " for " + Game.actions[i] + "\n"
+    embed.add_field(name="Actions", value=actionStr, inline=False)
+    playerObj = await game.guild.fetch_member(player.userid)
+    handMsg = await playerObj.send(embed=embed)
+    # React with all the actions
+    for actionReaction in Game.actionReactions:
+        await handMsg.add_reaction(actionReaction)
